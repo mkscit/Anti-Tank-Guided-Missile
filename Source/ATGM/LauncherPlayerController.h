68baba8feebcf56283f5c225036f6dc2ddf3575f
Mohammed Abushawish <59448589+mkscit@users.noreply.github.com>
2020-01-03 16:13:51 +0200

    Creating Launcher PlayerController

---
+++ Source/ATGM/LauncherPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LauncherPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ATGM_API ALauncherPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
