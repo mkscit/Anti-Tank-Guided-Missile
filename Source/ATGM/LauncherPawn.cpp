4f3cc8975a33d690a6425c7fde3742c134c804a8
Mohammed Abushawish <59448589+mkscit@users.noreply.github.com>
2020-01-03 16:29:33 +0200

    Updating Lancher Pawn

--- Source/ATGM/LauncherPawn.cpp
+++ Source/ATGM/LauncherPawn.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "LauncherPawn.h"
 
@@ -9,13 +7,34 @@ ALauncherPawn::ALauncherPawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	PlatfomSystem = CreateDefaultSubobject<UStaticMeshComponent>(FName("PlatfomSystem"));
+	LaserGuidingSystem = CreateDefaultSubobject<UStaticMeshComponent>(FName("LaserGuidingSystem"));
+	Launcher = CreateDefaultSubobject<UStaticMeshComponent>(FName("Launcher"));
+	MissileLocation = CreateDefaultSubobject<UStaticMeshComponent>(FName("Missile"));
+
+	SetRootComponent(PlatfomSystem);
+	LaserGuidingSystem->SetupAttachment(PlatfomSystem);
+	Launcher->SetupAttachment(LaserGuidingSystem);
+	MissileLocation->SetupAttachment(Launcher);
+
 }
 
 // Called when the game starts or when spawned
 void ALauncherPawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	TArray<UActorComponent*> Cameras = 
+		GetComponentsByClass(UCameraComponent::StaticClass());
+
+
+	if (Cameras.Num() > 0) {
+		ThirdPersonCAM2 = Cast<UCameraComponent>(Cameras[1]);
+		ThirdPersonCAM1   = Cast<UCameraComponent>(Cameras[0]);
+		FirstPersonCAM = Cast<UCameraComponent>(Cameras[2]);
+
+		
+	}
 }
 
 // Called every frame
@@ -30,5 +49,102 @@ void ALauncherPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("Right_Left", this, &ALauncherPawn::Right_Left);
+	PlayerInputComponent->BindAxis("Up_Down", this, &ALauncherPawn::Up_Down);
+	PlayerInputComponent->BindAxis("ZoomIn", this, &ALauncherPawn::ZoomInOut);
+	PlayerInputComponent->BindAxis("ZoomOut", this, &ALauncherPawn::ZoomInOut);
+	PlayerInputComponent->BindAction("ShowFirstPersonCAM", EInputEvent::IE_Pressed, this, &ALauncherPawn::ShowFirstPersonCAM);
+	PlayerInputComponent->BindAction("ShowThirdPersonCAM1", EInputEvent::IE_Pressed, this, &ALauncherPawn::ShowThirdPersonCAM1);
+	PlayerInputComponent->BindAction("ShowThirdPersonCAM2", EInputEvent::IE_Pressed, this, &ALauncherPawn::ShowThirdPersonCAM2);
+	PlayerInputComponent->BindAction("Fire", EInputEvent::IE_Pressed, this, &ALauncherPawn::Fire);
 }
+
+void ALauncherPawn::Right_Left(float Axis)
+{
+	if(!FirstPersonCAM || !ThirdPersonCAM1 || !ThirdPersonCAM2 || !LaserGuidingSystem) return;
+	
+	float Yaw = Axis * GetWorld()->DeltaTimeSeconds * CameraRotationSpeed;
+	FRotator Rotation(0, Yaw, 0);
+
+	LaserGuidingSystem->AddRelativeRotation(Rotation);
+}
+
+void ALauncherPawn::Up_Down(float Axis)
+{
+	if(!FirstPersonCAM || !ThirdPersonCAM1 || !ThirdPersonCAM2 || !LaserGuidingSystem) return;
+
+	float Pitch = Axis * GetWorld()->DeltaTimeSeconds * CameraRotationSpeed;
+	FRotator Rotation(Pitch, 0, 0);
+
+	LaserGuidingSystem->AddRelativeRotation(Rotation);
+}
+
+void ALauncherPawn::ZoomInOut(float Axis)
+{
+	if (!FirstPersonCAM) return;
+
+	if(FirstPersonCAM->IsActive())
+	{
+		float ZoomDegree = Axis * GetWorld()->DeltaTimeSeconds * CameraRotationSpeed;
+		ZoomDegree = ZoomDegree + FirstPersonCAM->FieldOfView;
+		ZoomDegree = FMath::Clamp<float>(ZoomDegree, 20, 90);
+		FirstPersonCAM->SetFieldOfView(ZoomDegree);
+	}
+}
+
+void ALauncherPawn::Fire()
+{
+	if(!FirstPersonCAM) return;
+
+	if(FirstPersonCAM->IsActive())
+	{
+		FVector Location = MissileLocation->GetComponentLocation();
+		FRotator Rotation = Launcher->GetComponentRotation();
+
+		// if (!ensure(MissileActor_BP)) return;
+
+		SetMissleInTheWorld(Location, Rotation);
+	}
+}
+
+void ALauncherPawn::ShowFirstPersonCAM()
+{
+	if (!FirstPersonCAM && !ThirdPersonCAM1 && !ThirdPersonCAM2) return;
+	FirstPersonCAM->Activate(true);
+	ThirdPersonCAM1->Deactivate();
+	ThirdPersonCAM2->Deactivate();
+}
+
+void ALauncherPawn::ShowThirdPersonCAM1()
+{
+	if (!FirstPersonCAM && !ThirdPersonCAM1 && !ThirdPersonCAM2) return;
+	ThirdPersonCAM1->Activate(true);
+	FirstPersonCAM->Deactivate();
+	ThirdPersonCAM2->Deactivate();
+}
+
+void ALauncherPawn::ShowThirdPersonCAM2()
+{
+	if (!FirstPersonCAM && !ThirdPersonCAM1 && !ThirdPersonCAM2) return;
+	ThirdPersonCAM2->Activate(true);
+	FirstPersonCAM->Deactivate();
+	ThirdPersonCAM1->Deactivate();
+}
+
+void ALauncherPawn::SetMissleInTheWorld(FVector Location, FRotator Rotation)
+{
+	// Missile = GetWorld()->SpawnActor<AMissileActor>(
+	// 	MissileActor_BP,
+	// 	Location,
+	// 	Rotation
+	// );
+}
+
+void ALauncherPawn::GetCameraLocation(FVector& CameraLocation)
+{
+	if (FirstPersonCAM)
+		CameraLocation = FirstPersonCAM->GetComponentLocation();
+}
+
+
 
--- Source/ATGM/LauncherPawn.h
+++ Source/ATGM/LauncherPawn.h
@@ -2,28 +2,80 @@
 
 #pragma once
 
+// #include "MissileActor.h"
+#include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Engine/World.h"
+
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "LauncherPawn.generated.h"
+
+class ALauncherPlayerController;
+// class AMissileActor;
 
 UCLASS()
 class ATGM_API ALauncherPawn : public APawn
 {
 	GENERATED_BODY()
 
-public:
-	// Sets default values for this pawn's properties
-	ALauncherPawn();
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
+	// Sets default values for this pawn's properties
+	ALauncherPawn();
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void GetCameraLocation(FVector& CameraLocation);
+
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Setup")
+	// 	TSubclassOf<AMissileActor> MissileActor_BP;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Setup")
+		float CameraRotationSpeed = 10;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UStaticMeshComponent* Launcher = nullptr;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UStaticMeshComponent* LaserGuidingSystem = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UStaticMeshComponent* PlatfomSystem = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UStaticMeshComponent* MissileLocation = nullptr;
+
+	UCameraComponent* FirstPersonCAM = nullptr;
+	
+	UCameraComponent* ThirdPersonCAM1 = nullptr;
+
+	UCameraComponent* ThirdPersonCAM2 = nullptr;
+
+
+
+	// AMissileActor* Missile = nullptr;
+
+	void Right_Left(float Axis);
+	void Up_Down(float Axis);
+	void ZoomInOut(float Axis);
+	void Fire();
+	
+	void ShowFirstPersonCAM();
+	void ShowThirdPersonCAM1();
+	void ShowThirdPersonCAM2();
+
+	void SetMissleInTheWorld(FVector Location, FRotator Rotation);
+
+	ALauncherPlayerController* LauncherPlayerController = nullptr;
 
 };
