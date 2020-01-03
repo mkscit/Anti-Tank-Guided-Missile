83642c0b1fbc042485d8674219a8b661e884d546
Mohammed Abushawish <59448589+mkscit@users.noreply.github.com>
2020-01-03 21:20:12 +0200

    updates

--- Source/ATGM/TankActor.cpp
+++ Source/ATGM/TankActor.cpp
@@ -32,9 +32,8 @@ void ATankActor::DestroyWeakArea1(AActor* AttackSource)
 	if(!Missile) return;
 
 	TArray<UActorComponent*> StaticMeshes = 
-		Tank->GetComponentsByClass(UStaticMeshComponent::StaticClass());
+		GetComponentsByClass(UStaticMeshComponent::StaticClass());
 
-	// UE_LOG(LogTemp, Warning, TEXT("Count =  %d"), StaticMeshes.Num());
 	if (StaticMeshes.Num() > 0) {
 		UE_LOG(LogTemp, Warning, TEXT("0 is %s"), *StaticMeshes[0]->GetName());
 		UE_LOG(LogTemp, Warning, TEXT("1 is %s"), *StaticMeshes[1]->GetName());
@@ -45,6 +44,17 @@ void ATankActor::DestroyWeakArea1(AActor* AttackSource)
 		UE_LOG(LogTemp, Warning, TEXT("6 is %s"), *StaticMeshes[6]->GetName());
 		UE_LOG(LogTemp, Warning, TEXT("7 is %s"), *StaticMeshes[7]->GetName());
 		UE_LOG(LogTemp, Warning, TEXT("8 is %s"), *StaticMeshes[8]->GetName());
+
+
+		// LogTemp: Warning: 0 is Body
+		// LogTemp: Warning: 1 is Light2
+		// LogTemp: Warning: 2 is Barrel
+		// LogTemp: Warning: 3 is Light1
+		// LogTemp: Warning: 4 is Commander
+		// LogTemp: Warning: 5 is Turret
+		// LogTemp: Warning: 6 is GunnerSight
+		// LogTemp: Warning: 7 is RightTrack
+		// LogTemp: Warning: 8 is LeftTrack
 	}
 	
 }
@@ -55,6 +65,9 @@ void ATankActor::DestroyWeakArea2(AActor* AttackSource)
 	Missile = Cast<AMissileActor>(AttackSource);
 
 	if(!Missile) return;
+
+	TArray<UActorComponent*> StaticMeshes = 
+		GetComponentsByClass(UStaticMeshComponent::StaticClass());
 
 	if (StaticMeshes.Num() > 0) {
 		UE_LOG(LogTemp, Warning, TEXT("0 is %s"), *StaticMeshes[0]->GetName());
