7dd470f00339fc01bcdd0827256b057e85f978f7
Mohammed Abushawish <59448589+mkscit@users.noreply.github.com>
2020-01-03 16:22:59 +0200

    Writing Code of Launcher PlayerController

--- Source/ATGM/LauncherPlayerController.cpp
+++ Source/ATGM/LauncherPlayerController.cpp
@@ -1,5 +1,47 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "LauncherPlayerController.h"
 
+void ALauncherPlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	AimAt();
+}
+
+void ALauncherPlayerController::AimAt()
+{
+	int32 OUT SizeX;
+	int32 OUT SizeY;
+	GetViewportSize(SizeX, SizeY);
+
+	FVector2D CrosshairPositionOnScreen = FVector2D(SizeX * 0.5, SizeY * 0.5);
+
+
+	if (DeprojectScreenPositionToWorld(CrosshairPositionOnScreen.X,
+		CrosshairPositionOnScreen.Y, Crosshair_3D_Location, Crosshair_3D_Direction)) {
+
+		if (!GetKorniteOperatorPawn()) return;
+
+		FVector OUT CameraLocation;
+		// GetKorniteOperatorPawn()->GetCameraLocation(CameraLocation);
+        
+
+		auto StartLocation = CameraLocation;
+		auto EndLocation = StartLocation + Crosshair_3D_Direction * Range;
+		// DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, -1, 0, 10);
+
+		LineTracing(StartLocation, EndLocation);
+	}
+}
+
+void ALauncherPlayerController::LineTracing(FVector& StartLocation, FVector& EndLocation)
+{
+	GetWorld()->LineTraceSingleByChannel(Hit, StartLocation, EndLocation, ECollisionChannel::ECC_Visibility);
+	
+	// auto HitActor = Hit.GetActor();
+	// if(HitActor) UE_LOG(LogTemp, Warning, TEXT("Line Tracing hits %s"), *HitActor->GetName());
+}
+
+ALauncherPawn* ALauncherPlayerController::GetKorniteOperatorPawn() const
+{
+	return Cast<ALauncherPawn>(GetPawn());
+}
--- Source/ATGM/LauncherPlayerController.h
+++ Source/ATGM/LauncherPlayerController.h
@@ -1,17 +1,33 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
+#include "LauncherPawn.h"
+#include "Public/DrawDebugHelpers.h"
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "LauncherPlayerController.generated.h"
 
-/**
- * 
- */
+class ALauncherPawn;
 UCLASS()
 class ATGM_API ALauncherPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void Tick(float DeltaSeconds) override;
+
+	FVector OUT Crosshair_3D_Direction;
+	FVector OUT Crosshair_3D_Location;
+	
+	FHitResult OUT Hit;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Setup")
+		float Range = 400000;
+
+	ALauncherPawn* GetKorniteOperatorPawn() const;
+
+	void AimAt();
+	void LineTracing(FVector& StartLocation, FVector& EndLocation);
 };
