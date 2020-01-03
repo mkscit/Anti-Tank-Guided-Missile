11e641af3f5b82a800eba30fff761c9277890633
Mohammed Abushawish <59448589+mkscit@users.noreply.github.com>
2020-01-03 20:50:50 +0200

    Update TankActor.cpp

--- Source/ATGM/TankActor.cpp
+++ Source/ATGM/TankActor.cpp
@@ -31,6 +31,21 @@ void ATankActor::DestroyWeakArea1(AActor* AttackSource)
 
 	if(!Missile) return;
 
+	TArray<UActorComponent*> StaticMeshes = 
+		Tank->GetComponentsByClass(UStaticMeshComponent::StaticClass());
+
+	// UE_LOG(LogTemp, Warning, TEXT("Count =  %d"), StaticMeshes.Num());
+	if (StaticMeshes.Num() > 0) {
+		UE_LOG(LogTemp, Warning, TEXT("0 is %s"), *StaticMeshes[0]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("1 is %s"), *StaticMeshes[1]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("2 is %s"), *StaticMeshes[2]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("3 is %s"), *StaticMeshes[3]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("4 is %s"), *StaticMeshes[4]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("5 is %s"), *StaticMeshes[5]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("6 is %s"), *StaticMeshes[6]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("7 is %s"), *StaticMeshes[7]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("8 is %s"), *StaticMeshes[8]->GetName());
+	}
 	
 }
 
@@ -41,5 +56,15 @@ void ATankActor::DestroyWeakArea2(AActor* AttackSource)
 
 	if(!Missile) return;
 
-	
+	if (StaticMeshes.Num() > 0) {
+		UE_LOG(LogTemp, Warning, TEXT("0 is %s"), *StaticMeshes[0]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("1 is %s"), *StaticMeshes[1]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("2 is %s"), *StaticMeshes[2]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("3 is %s"), *StaticMeshes[3]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("4 is %s"), *StaticMeshes[4]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("5 is %s"), *StaticMeshes[5]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("6 is %s"), *StaticMeshes[6]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("7 is %s"), *StaticMeshes[7]->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("8 is %s"), *StaticMeshes[8]->GetName());
+	}
 }
